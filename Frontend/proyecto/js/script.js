4a7dbff592ff261dafbdcdf191465f717381be26
DavidA0456 <davidadrian.dorobantulatascu@riberadeltajo.es>
2025-12-15 17:54:04 +0100

    Add files via upload

---
+++ Frontend/proyecto/js/script.js
@@ -0,0 +1,12 @@
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+
+//* Mostrar contraseña
+checkbox.addEventListener('click', ()=> {
+    if (checkbox.checked) {
+        password.type = "text"; 
+    }else{
+        password.type = "password"
+    }
+})
+
